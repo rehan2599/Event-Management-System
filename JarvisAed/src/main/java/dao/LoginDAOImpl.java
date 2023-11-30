9e399c3164d70bde2b6b218dc3dc08eb656a71c8
AakashSanganiNeu <sangani.aak@northeastern.edu>
2023-11-30 11:24:56 -0500

    dao updates

---
+++ JarvisAed/src/main/java/dao/LoginDAOImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import model.User;
+import exceptions.ExceptionHandling;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import service.ILoginService;
+import util.DbUtil;
+
+/**
+ *
+ * @author aakashsangani
+ */
+public class LoginDAOImpl implements ILoginService {
+    User user;
+    
+    public LoginDAOImpl(User user){
+        this.user = user;
+       
+    }
+    
+    @Override
+    public void RegisteredUser() {
+        try {
+            Connection conn = DbUtil.getConnection();
+            PreparedStatement prep = conn.prepareStatement(IQueryMapper.INSERT_USERS);
+            prep.setString(1, user.getEmail());
+            prep.setString(2, user.getPassword());
+            
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Login Successful");
+            
+        } catch (ExceptionHandling | SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        
+        
+    } 
+}
